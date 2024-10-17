984a49be7eec85875df633f68e5ce3e87ba75261
anwar <anwarnasir0970@gmail.com>
2024-10-17 00:22:03 -0900

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
